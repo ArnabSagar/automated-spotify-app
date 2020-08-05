89ea7754a8be52f6744e74ef5718cd92e62782a9
Arnab Sagar <arnabsagar@localhost.localdomain>
2020-08-04 21:36:56 -0400

    click login

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome()
 driver.get("https://open.spotify.com")
 print(driver.title)
 
 
-log_in = driver.find_element_by_class_name(_2221af4e93029bedeab751d04fab4b8b-scss _1edf52628d509e6baded2387f6267588-scss)
+log_in = driver.find_element_by_css_selector("#main > div > div.Root__top-container > div.Root__top-bar > header > div:nth-child(4) > button._2221af4e93029bedeab751d04fab4b8b-scss._1edf52628d509e6baded2387f6267588-scss")
+log_in.click()
