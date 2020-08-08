0e2ed7546e6a1b2960377ee43d48484997311c28
Arnab Sagar <arnabsagar@localhost.localdomain>
2020-08-07 22:28:58 -0400

    update 2

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
+from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://open.spotify.com")
-print(driver.title)
-
 
 log_in = driver.find_element_by_css_selector("#main > div > div.Root__top-container > div.Root__top-bar > header > div:nth-child(4) > button._2221af4e93029bedeab751d04fab4b8b-scss._1edf52628d509e6baded2387f6267588-scss")
 log_in.click()
+sleep(3)
+#**************************************
+
+
+email_entry = driver.find_element_by_xpath("//*[@id='login-username']")
+email_entry.send_keys("chuckycheese99")
+
+password_entry = driver.find_element_by_css_selector("#login-password")
+
+
+#**************************************
+login_button = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/form/div[3]/div[2]/button")
+login_button.click()
