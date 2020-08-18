74b265d26826e15fae71568286ff7eee6f2df9f8
Arnab Sagar <arnab.sagar47@gmail.com>
2020-08-17 23:40:28 -0400

    Cant get Popen to work

--- dhcp-traffic-monitor.py
+++ dhcp-traffic-monitor.py
@@ -6,6 +6,7 @@ from decouple import config
 IP_NETWORK = config('IP_NETWORK')
 IP_DEVICE = config('IP_DEVICE')
 
+"""
 proc = subprocess.Popen(['ping', IP_NETWORK], stdout=subprocess.PIPE)
 
 while True:
@@ -18,6 +19,15 @@ while True:
     connected_ip = line.decode('utf-8').split()[3]
 
     if connected_ip == IP_DEVICE:
+        print("Arnab\'s home")
         subprocess.Popen(['say', 'Arnab\'s phone just connected'])
 
 #To sniff the DHCP traffic for the device to reconnect
+
+
+"""
+
+proc1 = subprocess.Popen(['sudo','arp-scan', '-l','-r 3'], stdout=subprocess.PIPE)
+
+line = proc1.stdout.readline()
+print(line)
