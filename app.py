c0da82b670a631f7069a4da49b53f059d099b4a1
Arnab Sagar <arnab.sagar47@gmail.com>
2020-08-24 23:27:19 -0400

    linked scripts together

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 from decouple import config
 
+
 USERNAME = config('SPOTIFY_USERNAME')
 PASSWORD = config('SPOTIFY_PASSWORD')
  
 # webdriver setup and accessing website 
 
+
 driver = webdriver.Chrome()
 driver.get("https://open.spotify.com")
 log_in = driver.find_element_by_xpath("//*[@id='main']/div/div[2]/div[1]/header/div[4]/button[2]")
 log_in.click()
-sleep(5)
+sleep(2)    
 
 # Login screen
 email_entry = driver.find_element_by_xpath("//*[@id='login-username']")
@@ -37,6 +39,10 @@ playlist_name = driver.find_element_by_xpath('//*[@id="main"]/div/div[2]/div[2]/
 playlist_name.click()
 
 sleep(2)
+play_button = driver.find_element_by_xpath('//*[@id="main"]/div/div[2]/div[4]/div[1]/div/div[2]/div/div/div[2]/section/div[3]/div/button')
+play_button.click()
+
+sleep(5)
 device_picker = driver.find_element_by_xpath('//*[@id="main"]/div/div[2]/div[3]/footer/div/div[3]/div/div/div[2]')
 device_picker.click()
 
@@ -44,6 +50,4 @@ sleep(2)
 speaker = driver.find_element_by_xpath('//*[@id="main"]/div/div[2]/div[3]/footer/div/div[3]/div/div/div[2]/span/div/div/ul/button[2]/div[2]/div')
 speaker.click()
 
-sleep(2)
-play_button = driver.find_element_by_xpath('//*[@id="main"]/div/div[2]/div[4]/div[1]/div/div[2]/div/div/div[2]/section/div[3]/div/button')
-play_button.click()
+
--- dhcp-traffic-monitor.py
+++ dhcp-traffic-monitor.py
@@ -2,44 +2,80 @@ import os
 import sys
 import subprocess
 from decouple import config
+from time import  sleep
+#import app
 
 IP_NETWORK = config('IP_NETWORK')
 IP_DEVICE = config('IP_DEVICE')
-SUDOPASS = config('sudoPass')
 
-"""
+
 proc = subprocess.Popen(['ping', IP_NETWORK], stdout=subprocess.PIPE)
 
 while True:
-    line = proc.stdout.readline()
-    print(line)
     
+    line = proc.stdout.readline()
+
     if not line:
+        flag = 1
         break
         
     connected_ip = line.decode('utf-8').split()[3]
+    print(connected_ip)
+    sleep(2)
+    
+    new_IP = IP_NETWORK + ":"
+    print(new_IP)
 
-    if connected_ip == IP_DEVICE:
-        print("Arnab\'s home")
-        subprocess.Popen(['say', 'Arnab\'s phone just connected'])
+    if connected_ip == new_IP:
+        flag = 0
+        break
+        
+
+if(flag == 0):
+    print("Arnab\'s home")
+    
+    subprocess.call(["python3", "app.py"])
+    #app.spotifyBot()
+
+else:
+    print('Arnab\'s not home')
 
 #To sniff the DHCP traffic for the device to reconnect
 
 
 """
 
-#use - sudo arp-scan -l -r 3 | grep 192.168.0.11(The IP network of the device)
+#use - sudo arp-scan -l -r 3 | grep 192.168.0.11(The IP network of the device) 'grep', IP_NETWORK
 command = 'sudo arp-scan -l -r 3 | grep 192.168.0.11'
-proc = subprocess.Popen(['sudo','arp-scan', '-l', '-r 3','grep', IP_NETWORK], stdout=subprocess.PIPE)
+
+
+proc = subprocess.Popen(['sudo','arp-scan', '-l', '-r 3',], stdout=subprocess.PIPE)
 
 
 
 
 while True:
+
     line = proc.stdout.readline()
+    ip_save = []
+
+    for ch in line:
+        
+        if(ch == '\t'):
+            break
+        else:
+            ip_save.append(str(ch))
+
+    
+    #print(ip_save)
+
     print(line)
+    
+    
+    connected_ip = line.decode('utf-8').split()[0]
+    
     
     if not line:
         break
 
-        
+        """
